e6aa61f0b664191ff7a71a842cf6da683be469bc
RASEM0n <negativ12fly@gmail.com>
2021-09-14 09:26:16 +0500

    initlal setup

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    plugins: ['@typescript-eslint', 'prettier', 'react-hooks'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'next/core-web-vitals',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+        'plugin:react-hooks/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        'arrow-body-style': ['error', 'always'],
+        'prefer-arrow-callback': 'error',
+        'arrow-spacing': 'error',
+        'prefer-promise-reject-errors': 'off',
+    },
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: ['stylelint-scss'],
+    extends: ['stylelint-config-recommended-scss'],
+    rules: {
+        indentation: ['tab'],
+        'color-hex-case': 'upper',
+    },
+}
